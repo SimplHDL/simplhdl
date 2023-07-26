844ea8befbf9343423f2a99995c58e739a291505
Rasmus Olsen <zordid+simplhdl@gmail.com>
2023-07-26 19:33:26 +0200

    Add verbose logging

--- src/simplhdl/__main__.py
+++ src/simplhdl/__main__.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 from pathlib import Path
 from . import __version__
 from .simplhdl import Simplhdl
@@ -20,11 +21,21 @@ def parse_arguments():
         action='version',
         version=f"SimplHDL version {__version__}"
     )
+    parser.add_argument(
+        '-v', '--verbose',
+        action='count',
+        default=0,
+        help="Increase verbosity"
+
+    )
     return parser.parse_args()
 
 
 def main():
     args = parse_arguments()
+    levels = [logging.WARNING, logging.INFO, logging.DEBUG, logging.NOTSET]
+    level = levels[min(args.verbose, len(levels)-1)]
+    logging.basicConfig(level=level)
     load_plugins()
     simpl = Simplhdl()
     simpl.create_project(args.filespec)
