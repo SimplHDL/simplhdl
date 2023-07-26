ca854c30f6b72733125a15f26b0886b17acae456
Rasmus Olsen <zordid+simplhdl@gmail.com>
2023-07-26 19:33:26 +0200

    Update parsers

--- src/simplhdl/parser.py
+++ src/simplhdl/parser.py
@@ -1,19 +1,20 @@
 from pathlib import Path
 from typing import Callable
 from abc import ABCMeta, abstractmethod
+from pyEDAA.ProjectModel import FileSet
 
 
 class ParserBase(metaclass=ABCMeta):
 
-    def __init__(self, filename: Path, **kwargs):
-        self.filename = filename
+    def __init__(self, **kwargs):
+        pass
 
     @abstractmethod
     def is_valid_format(self):
         pass
 
     @abstractmethod
-    def parse(self):
+    def parse(self, filename: Path) -> 'FileSet':
         pass
 
 
@@ -39,7 +40,7 @@ class ParserFactory:
     @classmethod
     def get_parser(cls, filename: Path, **kwargs) -> 'ParserBase':
         for parser_class in cls.registry.values():
-            parser = parser_class(filename, **kwargs)
-            if parser.is_valid_format():
+            parser = parser_class(**kwargs)
+            if parser.is_valid_format(filename):
                 return parser
         raise Exception(f"Couldn't find Parser for parsing {filename}")
--- src/simplhdl/parsers/fusesocparser.py
+++ src/simplhdl/parsers/fusesocparser.py
@@ -1,27 +1,30 @@
 import yaml
-
 from pathlib import Path
+from pyEDAA.ProjectModel import FileSet
+
 from ..parser import ParserFactory, ParserBase
 
 
 @ParserFactory.register()
 class FuseSocParser(ParserBase):
 
-    def __init__(self, filename: Path):
-        super().__init__(filename)
+    _format_id: str = "CAPI=2:"
 
-    def is_valid_format(self) -> bool:
-        with open(self.filename, 'r') as fp:
-            first_line = fp.readline()
-        if first_line.strip() == "CAPI=2:":
-            return True
-        else:
-            return False
+    def __init__(self):
+        super().__init__()
 
-    def parse(self) -> None:
+    def is_valid_format(self, filename: Path) -> bool:
+        with open(filename, 'r') as fp:
+            if fp.readline().strip() == self._format_id:
+                return True
+            else:
+                return False
+
+    def parse(self, filemame: str) -> FileSet:
         with open(self.filename, 'r') as fp:
             try:
                 spec = yaml.safe_load(fp)
             except yaml.YAMLError as e:
                 raise e
-            print(spec)
+        print(spec)
+        return FileSet("name")
