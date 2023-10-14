9ccc89670ad326e77bfab0d96bf36670e8b7db61
Rasmus Olsen <22944947+RasmusGOlsen@users.noreply.github.com>
2023-10-14 12:11:14 +0200

    Add support for Synopsys Vcs

--- src/simplhdl/flows/vcsflow.py
+++ src/simplhdl/flows/vcsflow.py
@@ -8,20 +8,23 @@ import shutil
 
 from argparse import Namespace
 from pathlib import Path
+from typing import Callable, Generator, List, Dict, Tuple
 from jinja2 import Environment, FileSystemLoader
-from hashlib import md5
-import pyEDAA.ProjectModel as pm  # type: ignore
+from pyEDAA.ProjectModel import (File, VerilogSourceFile,
+                                 SystemVerilogSourceFile, VHDLSourceFile)
 
 from ..project import Project
-from ..utils import sh, generate_from_template
+from ..fileset import FileSet
+from ..utils import sh, generate_from_template, md5sum, append_suffix
 from ..flow import FlowFactory, FlowBase
 from ..resources.templates import vcs as templates
+from ..cocotb import Cocotb
 
 logger = logging.getLogger(__name__)
 
 
 @FlowFactory.register('vcs')
-class QuestaFlow(FlowBase):
+class VcsFlow(FlowBase):
 
     @classmethod
     def parse_args(self, subparsers) -> None:
@@ -52,6 +55,12 @@ class QuestaFlow(FlowBase):
             help="Extra flags for Vcs simv command"
         )
         parser.add_argument(
+            '--vcs-flags',
+            default='',
+            action='store',
+            help="Extra flags for Vcs vcs command"
+        )
+        parser.add_argument(
             '--vhdlan-flags',
             default='',
             action='store',
@@ -74,18 +83,29 @@ class QuestaFlow(FlowBase):
             action='store_true',
             help="Generate a random seed to initialize random generator"
         )
+        parser.add_argument(
+            '--timescale',
+            default='1ns/1ps',
+            action='store',
+            help="Set the simulator timescale for Verilog"
+        )
 
-    def run(self, args: Namespace, project: Project, builddir: Path) -> None:
-        self.project = project
-        self.builddir = builddir
-        self.args = args
+    def __init__(self, name, args: Namespace, project: Project, builddir: Path):
+        super().__init__(name, args, project, builddir)
+        self.hdl_language = None
+        self.cocotb = Cocotb(project)
+
+    def run(self) -> None:
         self.configure()
         self.generate()
-        self.execute(args.step)
+        self.execute(self.args.step)
 
     def configure(self):
         os.makedirs(self.builddir, exist_ok=True)
         self.is_tool_setup()
+        os.environ['RANDOM_SEED'] = str(self.args.seed)
+        if self.cocotb.enabled:
+            os.environ['MODULE'] = self.cocotb.module()
 
     def generate(self):
         templatedir = resources_files(templates)
@@ -93,80 +113,173 @@ class QuestaFlow(FlowBase):
             loader=FileSystemLoader(templatedir),
             trim_blocks=True)
 
-        template = environment.get_template('Makefile.j2')
-        generate_from_template(template, self.builddir)
+        libraries = [lib.Name for lib in self.project.DefaultDesign.VHDLLibraries.values()]
+        defaultlib = 'work'
 
-        template = environment.get_template('project.mk.j2')
-        generate_from_template(
-            template,
-            self.builddir,
-            toplevels=self.project.DefaultDesign.TopLevel,
-            libraries=' '.join([lib.Name for lib in self.project.DefaultDesign.VHDLLibraries.values()]))
+        template = environment.get_template('Makefile.j2')
+        generate_from_template(template, self.builddir, vcs_flags=self.vcs_flags(), simv_flags=self.simv_flags())
 
         template = environment.get_template('synopsys_sim.setup.j2')
-        defaultlib = next(iter(self.project.DefaultDesign.VHDLLibraries.values()))
+        generate_from_template(template, self.builddir, defaultlib=defaultlib, libraries=libraries)
+
+        template = environment.get_template('project.mk.j2')
+        toplevels = ' '.join([t for t in self.project.DefaultDesign.TopLevel.split() if t != self.cocotb.module()])
         generate_from_template(
             template,
             self.builddir,
-            defaultlib=defaultlib,
-            libraries=self.project.DefaultDesign.VHDLLibraries.values())
+            toplevels=toplevels,
+            libraries=' '.join(libraries))
 
-        for fileset in self.project.DefaultDesign.FileSets.values():
-            name = md5(fileset.Name.encode()).hexdigest()
-            files = [f.Path for f in fileset.Files() if f.FileType == pm.VerilogSourceFile]
-            try:
-                library = fileset.VHDLLibrary.Name
-            except AttributeError:
-                # TODO: This is a workaround The default fileset is pm.FileSet
-                #       which is bugged. Because it is empty we don't need it
-                #       anyway and can just ignore it.
-                library = ''
-            if files:
-                template = environment.get_template('fileset.j2')
-                output = self.builddir.joinpath(f"{name}.verilog.fileset")
-                flags = f"-work {library} {self.args.vlogan_flags}"
-                generate_from_template(template, output, flags=flags, files=files)
-                template = environment.get_template('files.j2')
-                output = self.builddir.joinpath(f"{name}.verilog.files")
-                generate_from_template(template, output, target=f"{name}.verilog.fileset", files=files)
-            files = [f.Path for f in fileset.Files() if f.FileType == pm.SystemVerilogSourceFile]
-            if files:
-                template = environment.get_template('fileset.j2')
-                output = self.builddir.joinpath(f"{name}.systemverilog.fileset")
-                flags = f"-sverilog -work {library} {self.args.vlogan_flags}"
-                generate_from_template(template, output, flags=flags, files=files)
-                template = environment.get_template('files.j2')
-                output = self.builddir.joinpath(f"{name}.systemverilog.files")
-                generate_from_template(template, output, target=f"{name}.systemverilog.fileset", files=files)
-            files = [f.Path for f in fileset.Files() if f.FileType == pm.VHDLSourceFile]
-            if files:
-                template = environment.get_template('fileset.j2')
-                output = self.builddir.joinpath(f"{name}.vhdl.fileset")
-                flags = f"-vhdl08 -work {library} {self.args.vhdlan_flags}"
-                generate_from_template(template, output, flags=flags, files=files)
-                template = environment.get_template('files.j2')
-                output = self.builddir.joinpath(f"{name}.vhdl.files")
-                generate_from_template(template, output, target=f"{name}.vhdl.fileset", files=files)
+        if self.cocotb.enabled:
+            template = environment.get_template('cocotb.mk.j2')
+            generate_from_template(template, self.builddir, pythonpath=self.cocotb.pythonpath)
+            template = environment.get_template('pli.tab.j2')
+            generate_from_template(template, self.builddir)
+
+        walker = FileSetWalker()
+        fileset_makefiles: List[str] = list()
+        for fileset in walker.walk(self.project.DefaultDesign.DefaultFileSet, reverse=True):
+            for language in ['verilog', 'systemverilog', 'vhdl']:
+                fileset_makefiles += self.generate_fileset_makefiles(environment, language, fileset)
+        rules = self.generate_fileset_dependencies(fileset_makefiles)
+        template = environment.get_template('dependencies.mk.j2')
+        generate_from_template(template, self.builddir, rules=rules)
+
+    def generate_fileset_dependencies(self, filelist: List[Path]):
+        """Generate a dependency makefile for filesets. There a two type of
+           make rules.
+           1. the vhdl fileset depends in the verilog fileset, i.e. the verilog
+              filesets needs to be compiled first.
+           2. A fileset depends on its children filesets.
+
+        Args:
+            filelist (List[str]): List of generated makefile filesets.
+        """
+        walker = FileSetWalker()
+        rules: Dict[str, List[str]] = dict()
+        for fileset in walker.walk(self.project.DefaultDesign.DefaultFileSet, reverse=False):
+            name = md5sum(fileset.Name)
+            dependencies = []
+            for language_fileset in [f for f in filelist if f.stem.startswith(name)]:
+                for child in fileset.FileSets.values():
+                    child_name = md5sum(child.Name)
+                    dependencies += [append_suffix(f, '.com').name for f in filelist
+                                     if f.stem.startswith(child_name)]
+                    if dependencies:
+                        rules[append_suffix(language_fileset, '.com').name] = dependencies
+        return rules
+
+    def generate_fileset_makefiles(self, environment: Environment, language: str, fileset: FileSet) -> List[Path]:
+        table: Dict[str, Tuple[List[File], Callable]] = {
+            'verilog': ([VerilogSourceFile], self.vlogan_flags),
+            'systemverilog': ([SystemVerilogSourceFile], self.svlogan_flags),
+            'vhdl': ([VHDLSourceFile], self.vhdlan_flags),
+        }
+        filetypes, flags = table[language]
+        files = [f for f in fileset.GetFiles() if f.FileType in filetypes]
+        name = md5sum(fileset.Name)
+        base = self.builddir.joinpath(f"{name}-{language}")
+        generated: List[str] = list()
+        if files:
+            template = environment.get_template('files.j2')
+            generate_from_template(
+                template,
+                base.with_suffix('.files'),
+                target=base.with_suffix('.fileset'),
+                files=[f.Path.absolute() for f in files])
+            template = environment.get_template('fileset.j2')
+            output = base.with_suffix('.fileset')
+            includes = {f.Path.parent.absolute() for f in files if f.Path.suffix in ['.vh', '.svh']}
+            files = [f.Path.absolute() for f in files if f.Path.suffix not in ['.vh', '.svh']]
+            generate_from_template(template, output, flags=flags(fileset), includes=includes, files=files)
+            generated.append(output)
+        return generated
+
+    def svlogan_flags(self, fileset: FileSet) -> str:
+        library = self.get_library(fileset)
+        verbosity = '-q' if self.args.verbose == 0 else ''
+        verbosity = '-V' if self.args.verbose == 2 else verbosity
+        return f"-sverilog {verbosity} -work {library} {self.args.vlogan_flags}".strip()
+
+    def vlogan_flags(self, fileset: FileSet) -> str:
+        library = self.get_library(fileset)
+        verbosity = '-q' if self.args.verbose == 0 else ''
+        verbosity = '-V' if self.args.verbose == 2 else verbosity
+        return f"+v2k {verbosity} -work {library} {self.args.vlogan_flags}".strip()
+
+    def vhdlan_flags(self, fileset: FileSet) -> str:
+        library = self.get_library(fileset)
+        verbosity = '-q' if self.args.verbose == 0 else ''
+        verbosity = '-V' if self.args.verbose == 2 else verbosity
+        return f"-vhdl08 {verbosity} -work {library} {self.args.vhdlan_flags}".strip()
+
+    def vcs_flags(self) -> str:
+        flags = set()
+        if self.args.verbose == 0:
+            flags.add('-q')
+        if self.args.timescale:
+            flags.add(f"-timescale={self.args.timescale}")
+        if self.cocotb.enabled:
+            if self.hdl_language == 'vhdl':
+                raise NotImplementedError("Cocotb with VHDL top is not implemented")
+            else:
+                flags.add(f"+vpi -P pli.tab -load {self.cocotb.lib_name_path('vcs', 'vpi')}")
+                flags.add("-debug_access+r+w+nomemcbk -debug_region+cell+encrypt")
+                flags.add("-debug_acc+pp+f+dmptf")
+        for name, value in self.project.Generics.items():
+            flags.add(f"-pvalue+{name}={value}")
+        for name, value in self.project.Parameters.items():
+            flags.add(f"-pvalue+{name}={value}")
+        for name, value in self.project.Defines.items():
+            flags.add(f"+define+{name}={value}")
+        for name, value in self.project.PlusArgs.items():
+            flags.add(f"+{name}={value}")
+        return ' '.join(list(flags) + [self.args.vcs_flags])
+
+    def simv_flags(self) -> str:
+        flags = set()
+        flags.add(f"+ntb_random_seed={self.args.seed}")
+        return ' '.join(list(flags) + [self.args.simv_flags])
+
+    def get_library(self, fileset: FileSet) -> str:
+        try:
+            library = fileset.VHDLLibrary.Name
+        except AttributeError:
+            # TODO: This is a workaround The default fileset is FileSet
+            #       which is bugged. Because it is empty we don't need it
+            #       anyway and can just ignore it.
+            library = ''
+        return library
 
     def execute(self, step: str) -> None:
-        if self.args.gui:
-            sh(['make', 'gui'], cwd=self.builddir)
+        self.run_hooks('pre')
+        sh(['make', 'compile'], cwd=self.builddir, output=True)
+        if step == 'compile':
             return
-        sh(['make', step], cwd=self.builddir, output=True)
 
-    def cocotb(self):
-        if self.is_cocotb():
-            libpython = sh(['cocotb-config', '--libpython']).strip()
-            os.environ['LIBPYTHON_LOC'] = libpython
-            os.environ['GPI_EXTRA'] = f"{get_lib_name_path('fli', 'questa')}:cocotbfli_entry_point"
-            os.environ['MODULE'] = 'test_adder'
-            os.environ['PYTHONPATH'] = '/home/rgo/devel/cocotb-example/cores/adder.core/verif/cocotb'
-            os.environ['RANDOM_SEED'] = '1'
+        if self.cocotb.enabled:
+            for toplevel in [t for t in self.project.DefaultDesign.TopLevel.split() if t != self.cocotb.module()]:
+                # TODO: what should happend in Vcs?
+                # self.hdl_language = get_hdl_language(toplevel, directory=self.builddir)
+                # os.environ['SIMPLHDL_LANGUAGE'] = self.hdl_language
+                pass
 
-    def is_cocotb(self) -> bool:
-        if [True for f in self.project.DefaultDesign.Files() if f.FileType == pm.CocotbPythonFile]:
-            return True
-        return False
+        if self.args.gui:
+            command = ['make', 'gui']
+        else:
+            command = ['make', step]
+        sh(command, cwd=self.builddir, output=True)
+        if step == 'simulate':
+            self.run_hooks('post')
+
+    def run_hooks(self, name):
+        try:
+            for command in self.project.Hooks[name]:
+                logger.info(f"Running {name} hook: {command}")
+                sh(command.split(), cwd=self.builddir, output=True)
+        except KeyError:
+            # NOTE: Continue if no hook is registret
+            pass
 
     def is_tool_setup(self) -> None:
         if (shutil.which('vlogan') is None or
@@ -175,6 +288,36 @@ class QuestaFlow(FlowBase):
             raise Exception("Vcs is not setup correctly")
 
 
-def get_lib_name_path(interface: str, simulator: str) -> str:
-    lib_name_path = sh(['cocotb-config', '--lib-name-path', interface, simulator]).strip()
-    return lib_name_path
+def get_hdl_language(name: str, directory: Path = Path.cwd()) -> str:
+    """Get language of HDL module by inspecting the compiled libraries
+
+    Args:
+        name (str): Module/Entity name
+        directory (Path): Directory of library locations
+
+    Returns:
+        str: Verilog or VHDL
+    """
+    info = sh(['vdir', '-prop', 'top', name], cwd=directory)
+    if info.startswith('ENTITY'):
+        return "vhdl"
+    elif info.startswith('MODULE'):
+        return "verilog"
+    else:
+        raise Exception(f"Unknow info: {info}")
+
+
+class FileSetWalker:
+
+    def __init__(self):
+        self.__visited: List = list()
+
+    def walk(self, fileset: FileSet, reverse=False) -> Generator[FileSet, None, None]:
+        if fileset.Name not in self.__visited:
+            self.__visited.append(fileset.Name)
+            if reverse is False:
+                yield fileset
+            for child in fileset.FileSets.values():
+                yield from self.walk(child, reverse)
+            if reverse:
+                yield fileset
